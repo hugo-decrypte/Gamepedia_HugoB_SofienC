5b970e6b349a56fc127171a9a7bb3d13941b5681
bocchiarati <88000sofien@gmail.com>
2025-02-10 17:03:11 +0100

    QuestionF

--- src/Questions/f.php
+++ src/Questions/f.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use gamepedia\configuration\Base;
 use gamepedia\configuration\DB;
+use gamepedia\models\Game;
+use gamepedia\models\Personnage;
 
 
 DB::init();
@@ -12,3 +14,25 @@ Base::init();
 
 
 echo "<h1>Afficher (name , deck) les personnages du jeu 12342 ;</h1>";
+
+$persos = Personnage::select("name", "deck")->whereHas("game2character", function ($query) {
+    $query->where("game_id", "=", 12342);
+})->get();
+
+echo "<table border='1' style='border-collapse: collapse;'>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Deck</th>
+        </tr>
+    </thead>
+    <tbody>";
+
+foreach ($persos as $perso) {
+    echo "<tr>" .
+        "<td>" . $perso->name . "</td>" .
+        "<td>" . $perso->deck . "</td>" .
+        "</tr>";
+}
+
+echo "</tbody></table>";
