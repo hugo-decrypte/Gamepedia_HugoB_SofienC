2edbe8949b1af02f871cacc1859b85f6653b99aa
bocchiarati <88000sofien@gmail.com>
2025-02-10 16:11:55 +0100

    QuestionD

--- src/Questions/d.php
+++ src/Questions/d.php
@@ -5,9 +5,32 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use gamepedia\configuration\Base;
 use gamepedia\configuration\DB;
+use gamepedia\models\Game;
 
 
 DB::init();
 Base::init();
 
 echo "<h1>Lister 200 jeux à partir du 21173 ème </h1>";
+
+$games = Game::where("id", ">=", 21173)->select("name", "alias", "id")->take(200)->orderBy('id')->get();
+
+echo "<table border='1' style='border-collapse: collapse;'>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nom</th>
+            <th>Alias</th>
+        </tr>
+    </thead>
+    <tbody>";
+
+foreach ($games as $game) {
+    echo "<tr>" .
+        "<td>" . $game->id . "</td>" .
+        "<td>" . $game->name . "</td>" .
+        "<td>" . $game->alias . "</td>" .
+        "</tr>";
+}
+
+echo "</tbody></table>";
