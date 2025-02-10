bf7764a982df4673c912280e2a9fbb9d8f7f7db2
bocchiarati <88000sofien@gmail.com>
2025-02-10 15:51:15 +0100

    QuestionA

--- src/Questions/a.php
+++ src/Questions/a.php
@@ -5,9 +5,32 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use gamepedia\configuration\Base;
 use gamepedia\configuration\DB;
+use gamepedia\models\Game;
 
 
 DB::init();
 Base::init();
 
 echo "<h1>Lister les jeux dont le nom contient « Marine »</h1>";
+
+$games = Game::where('name', 'LIKE', '%Marine%')->select("name", "alias", "id")->get();
+
+echo "<table border='1' style='border-collapse: collapse;'>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nom</th>
+            <th>Alias</th>
+        </tr>
+    </thead>
+    <tbody>";
+
+foreach ($games as $game) {
+    echo "<tr>" .
+        "<td>" . $game->id . "</td>" .
+        "<td>" . $game->name . "</td>" .
+        "<td>" . $game->alias . "</td>" .
+        "</tr>";
+}
+
+echo "</tbody></table>";
