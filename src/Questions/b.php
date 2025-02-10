03eb74f8eade580ece1599ab01ad299431449051
bocchiarati <88000sofien@gmail.com>
2025-02-10 16:03:24 +0100

    QuestionB

--- src/Questions/b.php
+++ src/Questions/b.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use gamepedia\configuration\Base;
 use gamepedia\configuration\DB;
+use gamepedia\models\Company;
+use gamepedia\models\Game;
 
 
 DB::init();
@@ -12,3 +14,27 @@ Base::init();
 
 
 echo "<h1>Lister les compagnies installées en France ;</h1>";
+
+$companies = Company::where('location_country','like' ,'%France%')->select("name", "alias", "id", "location_country")->get();
+
+echo "<table border='1' style='border-collapse: collapse;'>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nom</th>
+            <th>Alias</th>
+            <th>country</th>
+        </tr>
+    </thead>
+    <tbody>";
+
+foreach ($companies as $company) {
+    echo "<tr>" .
+        "<td>" . $company->id . "</td>" .
+        "<td>" . $company->name . "</td>" .
+        "<td>" . $company->alias . "</td>" .
+        "<td>" . $company->location_country . "</td>" .
+        "</tr>";
+}
+
+echo "</tbody></table>";
